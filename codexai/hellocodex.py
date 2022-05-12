d4908b56e402d171189402ccc93d20f8448514b7
le LAN <58851935+lan-ensad@users.noreply.github.com>
2022-05-12 17:42:57 +0200

    Create hellocodex.py

---
+++ codexai/hellocodex.py
@@ -0,0 +1,10 @@
+#pip install openai
+#npm install openai
+
+import os
+import openai
+
+openai.organization = "org-0DKJSQNTkW5ZGi97EhgH2E9q"
+openai.api_key = "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"
+print(openai.Engine.list())
+
